5039bc74874a943b85d360313cb509ea45e8b1b7
Paige Knittel <plknit00@gmail.com>
2024-05-22 15:23:03 -0400

    got rid of error messages

--- main.cpp
+++ main.cpp
@@ -1,26 +1,25 @@
 #include "print_csi.h"
 #include <cmath>
 #include <cstdint>
+#include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
+
+// ***************** switch to bazel from cmake ******************
+// instructions online
 
 std::vector<std::vector<float>> read_input(std::string input_file) {
-  std::string input;
-  std::ofstream myfile;
+  std::string input_garbage;
+  std::ifstream myfile;
   int num_people;
   int num_trans;
   std::vector<std::string> names;
   std::vector<std::vector<float>> transactions;
   myfile.open(input_file);
 
-  // read the number of people in file
-  getline(myfile, num_people, " ");
-  // read info we don't need to store
-  getline(myfile, " ");
-  // read the number of lines in file
-  getline(myfile, num_trans, " ");
-  // read info we don't need to store
-  getline(myfile, "\n");
+  // read the number of people and transactions in file
+  myfile >> num_people >> input_garbage >> num_trans >> input_garbage;
 
   for (int i = 0; i < num_trans; i++) {
     std::string person_paying;
@@ -32,25 +31,17 @@ std::vector<std::vector<float>> read_input(std::string input_file) {
     std::string string_amount;
     float amount;
     // read the name of the person owing money
-    getline(myfile, person_paying, " ");
-    // read info we don't need to store
-    getline(myfile, " ");
     // read the name of the person getting money
-    getline(myfile, person_recieving, " ");
     // read the amount of money for this transaction
-    getline(myfile, string_amount, "\n");
+    myfile >> person_paying >> input_garbage >> person_recieving >>
+        input_garbage;
     // remove the $ and . characters from this string
     string_amount.substr(1, string_amount.length() - 2);
     // check string amount is now a valid float value
-    if (IsFloat(string_amount)) {
-      amount = std::stof(string_amount);
-    } else {
-      // DO SOMETHING
-      // exit entire function and error message
-    }
+    amount = std::stof(string_amount);
     // check if person paying and person recieving exists in names
     // if they dont, add them, and update their indices
-    for (int i = 0; i < names.length(); i++) {
+    for (int i = 0; i < names.size(); i++) {
       if (names[i] == person_paying) {
         person_pay_found = true;
         person_pay_index = i;
@@ -63,13 +54,13 @@ std::vector<std::vector<float>> read_input(std::string input_file) {
         exit;
       }
     }
-    if (!person_paying) {
-      person_pay_index = names.length() - 1;
-      names.append(person_paying);
+    if (!person_pay_found) {
+      person_pay_index = names.size() - 1;
+      names.push_back(person_paying);
     }
-    if (!person_recieving) {
-      person_pay_index = names.length() - 1;
-      names.append(person_recieving);
+    if (!person_recieve_found) {
+      person_pay_index = names.size() - 1;
+      names.push_back(person_recieving);
     }
     // update directed graph for transactions
     transactions[person_pay_index][person_recieve_index] = amount;
