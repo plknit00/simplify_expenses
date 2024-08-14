a0e5557efcfa6fdd2feefa730f4cce5d3615310c
Paige Knittel <plknit00@gmail.com>
2024-08-13 19:27:36 -0700

    updated index convert

--- src/graph.h
+++ src/graph.h
@@ -6,3 +6,35 @@
 #include <queue>
 #include <string>
 #include <vector>
+
+class Graph {
+ private:
+  std::vector<float> transactions;
+  std::vector<std::string> names;
+  std::string input_file;
+  std::string output_file;
+  int num_people;
+
+ public:
+  void set_input_file(std::string input_f);
+
+  void set_output_file(std::string output_f);
+
+  // create graph based on number of people
+  // initialize all transaction amounts to $0
+  void create_graph();
+
+  // read graph metadata
+  int read_metadata_input(std::ifstream &infile);
+
+  // read transactions from input file
+  void read_transactions(int num_trans, std::ifstream &infile);
+
+  void read_input();
+
+  // convert between [][] to vector indecies
+  int index_convert(int source_index, int sink_index);
+
+  // get amount that one person owes another
+  float source_owes_sink(int source_index, int sink_index);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,17 @@
 #include <vector>
 
 int main(int argc, char *argv[]) {
-  std::string input_file = argv[0];
-  std::string output_file = argv[1];
-  std::vector<std::string> names;
+  Graph transactions;
+  transactions.set_input_file(argv[0]);
+  transactions.set_output_file(argv[1]);
+  transactions.read_input();
+  transactions.create_graph();
   // Should I calculate net change in cash for each person to see if anyone owes
   // 0 overall before proceeding with maxflow?
   // eventually add dinic's optimization of cutting off dead ends ahead of time
-  std::vector<std::vector<float>> transactions = read_input(input_file, names);
-  std::vector<std::vector<float>> optimized_transactions =
-      compute_optimized_transactions(transactions);
-  print_output(names, optimized_transactions, output_file);
+  // std::vector<std::vector<float>> transactions = read_input(input_file,
+  // names); std::vector<std::vector<float>> optimized_transactions =
+  // compute_optimized_transactions(transactions);
+  // print_output(names, optimized_transactions, output_file);
   return 0;
 }
